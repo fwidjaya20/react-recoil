b6f775e870ba96ce4f0c660ed8dcfb5d22d449e2
Fredrick Widjaya <f.widjaya20@gmail.com>
2020-07-21 09:19:45 +0700

    Adding Style Loader

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,7 @@ module.exports = {
                     {
                         loader: MiniCssExtractPlugin.loader,
                     },
+                    "style-loader",
                     "css-loader",
                     "sass-loader"
                 ]
