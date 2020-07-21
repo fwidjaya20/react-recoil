2e330cb7a23cdba268b08732a7600700ff3316b8
Fredrick Widjaya <f.widjaya20@gmail.com>
2020-07-21 08:54:40 +0700

    Adding Webpack Plugin - webpack.DefinePlugin : for define environments - HtmlWebpackPlugin : for simplify creation of HTML files to serve webpack bundle - CleanWebpackPlugin : for remove / clean build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
+const dotenv = require("dotenv").config();
 const path = require("path");
+const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const PUBLIC_PATH = process.env.PUBLIC_PATH;
+const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
     name: "web",
     entry: "./app/web/src/index.ts",
     output: {
         path: path.join(__dirname, "/dist/web"),
-        filename: "bundle.min.js"
+        filename: NODE_ENV === "production" 
+            ? "[name].[contenthash].bundle.min.js"
+            : "[name].[hash].bundle.min.js",
+        chunkFilename: NODE_ENV === "production"
+            ? "[name].[contenthash].bundle.min.js"
+            : "[name].[hash].bundle.min.js",
+        publicPath: PUBLIC_PATH
     },
     resolve: {
         extensions: [".js", ".ts", ".tsx"],
@@ -24,5 +37,19 @@ module.exports = {
                 use: ['ts-loader'],
             },
         ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": JSON.stringify(dotenv.parsed)
+        }),
+        new CleanWebpackPlugin.CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html"),
+            minify: true
+        })
+    ],
+    devServer: {
+        hot: false,
+        publicPath: PUBLIC_PATH
     }
 };
